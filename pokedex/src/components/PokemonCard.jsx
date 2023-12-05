5911773a711b105b72aea151f48bb654f14de828
Baptiste <102555318+NovemIgnotum@users.noreply.github.com>
2023-12-05 16:56:51 +0100

    PokemonCard
    ajout PokemonCard

---
+++ pokedex/src/components/PokemonCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../style/PokemonCard.css'
+
+function  PokemonCard({props}) {
+    return(
+        <div className="card">
+            <h1>{props.name}</h1> 
+            <img className="pokedex-cover" src={props.cover} alt={`${name} cover`} />
+            {props.type}
+            {props.id}
+            {props.generation}
+        </div>
+    )
+
+}
+
+export default PokemonCard
