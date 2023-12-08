64344c33335f3947c628c4bb726b6a19461bbcd8
Farfadeli <ndaunac@gmail.com>
2023-12-08 10:10:03 +0100

    add pokecard to display

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -4,7 +4,7 @@ import PokemonCard from './components/PokemonCard';
 
 function App() {
   return (
-    <PokemonCard name="Pikachu" cover="https://www.pokepedia.fr/images/thumb/7/76/Pikachu-DEPS.png/250px-Pikachu-DEPS.png" type="Electrique" id="#025" generation="1"/>
+    <Display/>
   );
 }
 
--- pokedex/src/components/Display.jsx
+++ pokedex/src/components/Display.jsx
@@ -5,10 +5,9 @@ import PokemonCard from './PokemonCard'
 
 
 
-const Display  = (props) =>{
+const Display  = () =>{
 
     const [pokeList,setPokeList] = useState([])
-    const [cardPoke, setCardpoke] = useState({})
 
     async function fetchApi(url_pokemon){
         // Récupération des données (promesse)
@@ -18,24 +17,28 @@ const Display  = (props) =>{
         let res = []
         // Boucle sur toute les données récupérer
         pokemons["data"].map((elem) => {
-            res.push(elem["image"])
+            res.push({"id" : elem["id"] , "cover" :elem["image"], "name": elem["name"]["fr"], "generation": elem["generation"], "type": [elem["types"]]})
         })
-        setPokeList(res)
 
-        console.log(pokeList)
+        setPokeList(res)
     }
 
     useEffect(() => {
         const url_pokemon = "https://pokedex-api.3rgo.tech/api/pokemon"
         const url_type = "https://pokedex-api.3rgo.tech/api/types"
         fetchApi(url_pokemon)
-        console.log(cardPoke)
     }, [])
 
     return(
         <div className="display">
             <SearchBar/>
             <div className="pokeList">
+                {
+                pokeList.map((elem) => {
+                    return <PokemonCard name={elem["name"]} cover={elem["cover"]} id={elem["id"]} generation={elem["generation"]} type={elem["type"][0]}/>
+                })
+                
+                }
             </div>
         </div>
     )
