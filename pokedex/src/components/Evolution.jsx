efa98e40f234b4d2cbbe245f08142d3e2e4aa70f
Farfadeli <ndaunac@gmail.com>
2023-12-22 10:44:36 +0100

    sasasa

---
+++ pokedex/src/components/Evolution.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+
+const Evolution = ({evolve_before, evolve_after, condition}) => {
+    const [listId, setListID] = useState([])
+
+    useEffect(() => {
+        
+    }, [])
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default Evolution;
