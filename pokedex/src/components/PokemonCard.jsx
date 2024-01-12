8050837dc83e5d7a30e9747d865f4433b32fa173
Farfadeli <ndaunac@gmail.com>
2024-01-12 11:01:10 +0100

    opsushsisushqds

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -73,7 +73,11 @@ function PokemonCard({ name_type, name, cover, type, id, generation, height, wei
                             <h3>Poids : {weight}</h3>
                         </div>
                         <div className='card-details'>
+<<<<<<< Updated upstream
                             <RadarChart atk={atk} def={def} vit={vit} hp={hp} spe_atk={spe_atk} spe_def={spe_def}/>
+=======
+                            <RadarChart/>
+>>>>>>> Stashed changes
                         </div>
                     </div>
                 </div>
